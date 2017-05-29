9999cade29078c90f59fd09163b64084c1390981
lenny <Lenny@Clarity.fios-router.home>
2017-05-29 01:23:35 -0400

    Fixed a painful amount of bugs

--- PlayLimit.lua
+++ PlayLimit.lua
@@ -1,44 +1,38 @@
 local frame = CreateFrame("FRAME", "PlayLimitFrame");
-frame:RegisterEvent("ADDON_LOADED");
-
-local function eventHandler(self, event, arg1)
-	if event == "ADDON_LOADED" and arg1 == "PlayLimit" then
-		if PlayLimitDB == nil then
-			PlayLimitDB = {};
-			PlayLimitDB.endDate = getNewEndDate();
-			PlayLimitDB.gameTimer = 0; 
-		end
-	startPlayCounter();
-	end
-end
-
-frame:SetScript("OnEvent", eventHandler);
+frame:RegisterEvent("PLAYER_ENTERING_WORLD");
 
 function getNewEndDate()
-  local today = os.date("*t", os.time());
-  local endDate = os.time() + (8 - today.wday) * 24 * 3600;
+  local today = date("*t", time());
+  local endDate = time() + (8 - today.wday) * 24 * 3600;
   
   return endDate;
 end
 
-function startPlayCounter()
-	for (;;)
-		if (os.time() > PlayLimitDB.endDate) then
-  			print("start new timer and set end date to next sunday.");
+local function onEvent(self, event, ...)
+	if PlayLimitDB == nil then
+		PlayLimitDB = {};
+		PlayLimitDB.endDate = getNewEndDate();
+		PlayLimitDB.gameTimer = 0;	
+	end
+end
+
+local function onUpdate(self, elapsed)
+	self.elapsed = (self.elapsed or 0) + elapsed;
+	if self.elapsed >= 60 then
+		if (time() > PlayLimitDB.endDate) then
   			PlayLimitDB.endDate = getNewEndDate();
   			PlayLimitDB.gameTimer = 0;
 		end
-		sleep(60);
 		PlayLimitDB.gameTimer = PlayLimitDB.gameTimer + 60;
-		
-		if (os.time() < PlayLimitDB.endDate and PlayLimitDB.gameTimer >= 21600) then
-  			print('playtime has expired, kick player offline.');
+		if (time() < PlayLimitDB.endDate and PlayLimitDB.gameTimer >= 21600) then
+  			message('playtime has expired, time to log off');
   			Logout();
 		end
-	end
-
-	local function sleep(n)
-		local t0 = clock()
-  		while clock() - t0 <= n do end
+		self.elapsed = 0;
 	end
 end
+
+frame:SetScript("OnEvent", onEvent);
+frame:SetScript("OnUpdate", onUpdate);
+
+
